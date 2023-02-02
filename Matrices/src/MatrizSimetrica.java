e0b19a317ffb32ebfd49024b2b42dad43969a1f3
Mayii <mayicadavid.527@gmail.com>
2023-02-02 12:39:44 -0500

    ejemplos

---
+++ Matrices/src/MatrizSimetrica.java
@@ -0,0 +1,25 @@
+public class MatrizSimetrica {
+    public static void main(String[] args) {
+        boolean simetrica = true;
+        int[][] matriz = {
+                {1,2,3,4},
+                {2,1,0,5},
+                {3,0,1,6},
+                {4,5,8,7},
+        };
+
+        salir: for(int i = 0; i < matriz.length; i++){
+            for (int j = 0;j  < i; j++){
+                if (matriz[i][j] != matriz[j][i]){
+                    simetrica = false;
+                    break salir;
+                }
+            }
+        }
+        if (simetrica){
+            System.out.println("la matriz es simétrica");
+        }else{
+            System.out.println("no es simétrica");
+        }
+    }
+}
