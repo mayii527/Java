3e2219362ffd03bbbed4af45148600b9ec87c4f3
Mayii <mayicadavid.527@gmail.com>
2023-02-27 09:49:29 -0500

    add files

--- poo_interfaces/src/org/mayii/poointerfaces/imprenta/EjemploImprenta.java
+++ poo_interfaces/src/org/mayii/poointerfaces/imprenta/EjemploImprenta.java
@@ -1,7 +1,21 @@
 package org.mayii.poointerfaces.imprenta;
+import org.mayii.poointerfaces.imprenta.modelo.*;
 
 public class EjemploImprenta {
     public static void main(String[] args) {
 
+        Curriculum cv = new Curriculum("Mayii Cadavid","Dessarrolladora","Resumen laboral...");
+        cv.addExperiancia("Java");
+        cv.addExperiancia("Python");
+        cv.addExperiancia("JavaScript");
+        cv.addExperiancia("HTML");
+        cv.addExperiancia("CSS");
+
+        Informe informe = new Informe("Martin Fowler","James", "Estudio sobre microservicios");
+        imprimir(cv);
+        imprimir(informe);
+    }
+    public static void imprimir(Hoja imprimible){
+        System.out.println(imprimible.imprimir());
     }
 }
---
+++ poo_interfaces/src/org/mayii/poointerfaces/imprenta/modelo/Curriculum.java
@@ -0,0 +1,37 @@
+package org.mayii.poointerfaces.imprenta.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Curriculum extends Hoja{
+
+    private String persona;
+    private String carrera;
+    private List<String> experiancias;
+
+    public Curriculum(String persona, String carrera, String contenido) {
+        super(contenido);
+        this.persona = persona;
+        this.carrera = carrera;
+        this.experiancias = new ArrayList<>();
+    }
+
+    public Curriculum addExperiancia(String exp){
+        experiancias.add(exp);
+        return this;
+    }
+
+    @Override
+    public String imprimir() {
+        StringBuilder sb = new StringBuilder("Nombre: ");
+        sb.append(persona).append("\n")
+                .append("Resumen: ").append(contenido)
+                .append("\n").append("Profession: ")
+                .append(carrera).append("\n")
+                .append("Experiancia: ").append("\n");
+        for (String exp: experiancias){
+            sb.append("- ").append(exp).append("\n");
+        }
+        return sb.toString();
+    }
+}
