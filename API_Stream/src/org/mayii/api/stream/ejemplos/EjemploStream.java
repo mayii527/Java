0df208680d1d4500e873cfea93e0e377c65e8e58
Mayii <mayicadavid.527@gmail.com>
2023-04-13 11:53:07 -0500

    API Stream

--- API_Stream/src/org/mayii/api/stream/ejemplos/EjemploStream.java
+++ API_Stream/src/org/mayii/api/stream/ejemplos/EjemploStream.java
@@ -1,17 +1,29 @@
 package org.mayii.api.stream.ejemplos;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class EjemploStream {
     public static void main(String[] args) {
-        Stream<String> nombres = Stream.of("Mayii", "Sara", "Thali", "Fernando");
+        System.out.println("________________________ Tipo Stream__________________________");
+        Stream<String> nombres = Stream.of("Milú", "Milo", "Lizzy", "Molly");
         nombres.forEach(System.out::println);
 
-        System.out.println("__________________________________________________");
+        System.out.println("________________________Array__________________________");
 
-        String[] arr = {"Mayii", "Sara", "Thali", "Fernando"};
+        String[] arr = {"Milú", "Milo", "Lizzy", "Molly"};
         nombres = Arrays.stream(arr);
         nombres.forEach(System.out::println);
+
+        System.out.println("_______________________List___________________________");
+        List<String> lista = new ArrayList<>();
+        lista.add("Milú");
+        lista.add("Milo");
+        lista.add("Lizzy");
+        lista.add("Molly");
+
+        lista.stream().forEach(System.out::println);
     }
 }
