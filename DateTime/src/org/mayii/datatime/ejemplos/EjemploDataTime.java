994a89987dd5d0bd779f5463b50dfe3024c99d36
Mayii <mayicadavid.527@gmail.com>
2023-05-03 15:23:33 -0500

    Date Time

---
+++ DateTime/src/org/mayii/datatime/ejemplos/EjemploDataTime.java
@@ -0,0 +1,10 @@
+package org.mayii.datatime.ejemplos;
+
+import java.time.LocalDate;
+
+public class EjemploDataTime {
+    public static void main(String[] args) {
+        LocalDate fechaActual = LocalDate.now();
+        System.out.println("fechaActual = " + fechaActual);
+    }
+}
