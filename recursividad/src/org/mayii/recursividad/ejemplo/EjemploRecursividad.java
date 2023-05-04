e2b99225da9485eb7c2f6e388ddab73c405f5d4f
Mayii <mayicadavid.527@gmail.com>
2023-05-04 13:46:35 -0500

    Recursividad

---
+++ recursividad/src/org/mayii/recursividad/ejemplo/EjemploRecursividad.java
@@ -0,0 +1,50 @@
+package org.mayii.recursividad.ejemplo;
+
+import org.mayii.recursividad.ejemplo.models.Componente;
+
+public class EjemploRecursividad {
+    public static void main(String[] args) {
+
+        Componente pc = new Componente("Gabinete");
+        Componente poder = new Componente("Fuente poder 700w");
+        Componente placaMadre = new Componente("Asus sockest AMD");
+        Componente cpu = new Componente("Cpu AMD Ryzen 5 2800");
+        Componente ventilador = new Componente("Ventilador CPU");
+        Componente disipador = new Componente("Disipador");
+        Componente tv = new Componente("Nvidia RTX 3080 8GB");
+        Componente gpu = new Componente("Nvidia GPU RTX");
+        Componente gpuRAM = new Componente("4GB RAM");
+        Componente gpuRAM2 = new Componente("4GB RAM");
+        Componente gpuVentiladores = new Componente("Ventiladores");
+        Componente ram = new Componente("Memoria RAM 32GB");
+        Componente ssd = new Componente("Disco SSD 2T");
+
+        cpu.addComponente(ventilador)
+                .addComponente(disipador);
+
+        tv.addComponente(gpu)
+                .addComponente(gpuRAM)
+                .addComponente(gpuRAM2)
+                .addComponente(gpuVentiladores);
+        placaMadre.addComponente(cpu)
+                .addComponente(tv)
+                .addComponente(ram)
+                .addComponente(ssd);
+        pc.addComponente(poder)
+                .addComponente(placaMadre)
+                .addComponente(new Componente("Teclado"))
+                .addComponente(new Componente("Mouse"));
+
+        metodoRecursivo(pc, 0);
+
+    }
+
+    public static void metodoRecursivo(Componente c, int nivel) {
+        System.out.println("\t".repeat(nivel) + c.getNombre());
+        if (c.tieneHijos()) {
+            for (Componente hijo : c.getHijos()) {
+                metodoRecursivo(hijo, nivel + 1);
+            }
+        }
+    }
+}
